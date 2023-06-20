3f0ee2eb6f227bb6eae156f6a4dbd62de96e9e5b
Luan Carlos Macedo de Almeida <luancmadev@gmail.com>
2023-06-20 16:37:12 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,46 @@
+import { GetStaticProps } from "next";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({
+  dynamicUpdates,
+}: {
+  dynamicUpdates?: {
+    payload: {
+      body: {
+        version: number;
+        message: string;
+      };
+    };
+  };
+}) {
+  console.log(process.env.NEXT_PUBLIC_BACKEND);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing - build-2</h1>
+      <h1>Testing Build</h1>
+      <p className="text-2xl">
+        {dynamicUpdates?.payload.body.version || "No version"}
+      </p>
+      <p className="text-2xl">
+        {dynamicUpdates?.payload.body.message || "No message"}
+      </p>
     </main>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return axios
+    .post(`${process.env.NEXT_PUBLIC_BACKEND}/webhook`)
+    .then((response) => {
+      const data = response.data;
+      return {
+        props: {
+          dynamicUpdates: data,
+        },
+      };
+    })
+    .catch((error) => {
+      return {
+        props: {},
+      };
+    });
+};
