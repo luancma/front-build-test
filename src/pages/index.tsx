78adff3841beeba1a57c9b342f2cde0cbbbb8e2d
luancma <luancma@users.noreply.github.com>
2023-06-20 15:14:48 +0000

    deploy: deb967e6d9e48cccb322528aae7e37bbc2831c63

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing - build-2</h1>
-    </main>
-  );
-}
