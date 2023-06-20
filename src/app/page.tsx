d31cc7e900eea645b07e763532d05e71d6513eb1
Luan Carlos Macedo de Almeida <luancmadev@gmail.com>
2023-06-20 14:44:57 +0100

    --wip-- [skip ci]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing - build-1</h1>
+      <h1>Testing - build-2</h1>
     </main>
   );
 }
